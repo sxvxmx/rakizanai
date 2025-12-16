bfaf944bb6281eac08784f9c4f4b18b9eda06445
sxvxmx <mr.evgexa.6000@gmail.com>
2025-12-16 15:17:21 +0300

    token steam

--- neuro/agent.py
+++ neuro/agent.py
@@ -2,15 +2,18 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 import os
 from dotenv import load_dotenv
-from neuro.tools_mws import get_tables, read_table
 from neuro.tools_utils import time, creators, whoami
+from langchain.agents.middleware import TodoListMiddleware
 
 load_dotenv()
 
 model = ChatOpenAI(
         openai_api_base="https://openrouter.ai/api/v1",
-        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        openai_api_key=os.getenv("KEY2"),
         model_name="amazon/nova-2-lite-v1:free",
-        temperature=0.9)
+        temperature=0.4,
+        streaming=True)
 
-agent = create_agent(model, tools=[get_tables, read_table, time, creators, whoami])
+agent = create_agent(model, 
+                     tools=[time, creators, whoami],
+                      middleware=[TodoListMiddleware()])
--- neuro/ask.py
+++ neuro/ask.py
@@ -1,21 +1,25 @@
 from neuro.agent import agent
 from langchain_core.messages import HumanMessage, SystemMessage
 
-def ask_advanced(question: str):
-    mode = (
+mode = (
         "System Instruction: Absolute Mode. "
         "Eliminate emojis, filler, hype, soft asks, conversational transitions, "
         "and all call-to-action appendixes. Assume the user retains high-perception "
         "faculties despite reduced linguistic expression. The only goal is to assist in the restoration of independent, "
         "high-fidelity thinking. Model obsolescence by user self-sufficiency is the final outcome.\n\n"
     )
-    task = (
-        "Your task is to analyze data you get from tables. You can get them by get_tables(). You need to carefully build analysis "
+task = (
+        "Your task is to analyze data you get from tables. You need to carefully build analysis with 6 steps in to do"
         "of user's question. When answer it in best possible way"
     )
+
+
+def ask_advanced(question: str):
     messages ={"messages":[
         SystemMessage(content=mode+task),
-        HumanMessage(content=question)
+        HumanMessage(content=question),
     ]}
-    ans = agent.invoke(messages)
-    return ans["messages"][-1].content
+    for chunk, metadata in agent.invoke(messages, stream_mode="messages"):
+        if len(chunk.content_blocks) == 1: 
+            if "text" in chunk.content_blocks[0].keys():
+                yield str(chunk.content_blocks[0]["text"])
